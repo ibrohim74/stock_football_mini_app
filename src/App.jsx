f9c915489b49d566be3274e462fea8a3f2fb166e
ibrohim74 <xibroxim@gmail.com>
2024-09-11 01:18:11 +0500

    sad

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import IndexRouter from "./utils/IndexRouter.jsx";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {FOOTBALL, INDEX, TAP} from "./utils/const.jsx";
+import IndexPage from "./pages/index_page.jsx";
+import Layout from "./pages/layout.jsx";
 
 function App() {
     return (
 
-            <IndexRouter />
+        <BrowserRouter>
+            <Routes>
+                <Route path={INDEX} element={<IndexPage />} />
+                <Route path={`${FOOTBALL}/*`} element={<Layout path="football" />} />
+                <Route path={`${TAP}/*`} element={<Layout path="tap" />} />
+            </Routes>
+        </BrowserRouter>
 
     );
 }
