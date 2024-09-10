f03e6d1fab2d06d6d30ebf8fc91ebf042d552db4
ibrohim74 <xibroxim@gmail.com>
2024-09-11 01:08:18 +0500

    11.09

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
 import IndexRouter from "./utils/IndexRouter.jsx";
-import Layout from "./pages/layout.jsx";
 
 function App() {
     return (
