9312ae28cf1d06468a60caff8a9e90b1e6a06ac0
ibrohim74 <xibroxim@gmail.com>
2024-09-11 01:15:53 +0500

    11.092

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import React from "react";
 import IndexRouter from "./utils/IndexRouter.jsx";
 
 function App() {
     return (
-        <>
+
             <IndexRouter />
-        </>
+
     );
 }
 
