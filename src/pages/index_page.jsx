1528486c0bd3742b49bb31b35c1d88f846c78f7a
ibrohim74 <xibroxim@gmail.com>
2024-09-19 21:24:19 +0500

    sdsad

--- src/pages/index_page.jsx
+++ src/pages/index_page.jsx
@@ -3,7 +3,13 @@ import {Link, useParams} from "react-router-dom";
 import { FOOTBALL, HOME_PAGE_FOOTBALL, TAP, HOME_PAGE_TAP } from "../utils/const.jsx";
 
 const IndexPage = () => {
-    const {user_id } = useParams()
+    let {user_id } = useParams()
+
+    if (user_id){
+        user_id = parseInt(user_id);
+    }else {
+        user_id = 1
+    }
     return (
         <div className="index_page">
             user: {user_id}
