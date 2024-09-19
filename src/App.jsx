3a3630027a796e6a0056afe4496ac8a6dfd2ec62
ibrohim74 <xibroxim@gmail.com>
2024-09-19 21:27:16 +0500

    sdsad

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes, useParams} from "react-router-dom";
 import {FOOTBALL, INDEX, TAP} from "./utils/const.jsx";
 import IndexPage from "./pages/index_page.jsx";
 import Layout from "./pages/layout.jsx";
@@ -6,24 +6,26 @@ import {useEffect} from "react";
 
 function App() {
     const tg = window.Telegram.WebApp
-
+    const {user_id} = useParams()
     useEffect(() => {
         tg.expand()
     }, [])
 
-    return (
-        <BrowserRouter>
-            <Routes>
-                {/* Index page route */}
-                <Route path={`${INDEX}:user_id`} element={<IndexPage />} />
-
-                {/* Football route with dynamic user ID */}
-                <Route path={`${FOOTBALL}:user_id/*`} element={<Layout path="football" />} />
-
-                {/* Tap route with dynamic user ID */}
-                <Route path={`${TAP}:user_id/*`} element={<Layout path="tap" />} />
-            </Routes>
-        </BrowserRouter>
+    return (<> asd
+            {user_id}
+        </>
+        // <BrowserRouter>
+        //     <Routes>
+        //         {/* Index page route */}
+        //         <Route path={`${INDEX}:user_id`} element={<IndexPage />} />
+        //
+        //         {/* Football route with dynamic user ID */}
+        //         <Route path={`${FOOTBALL}:user_id/*`} element={<Layout path="football" />} />
+        //
+        //         {/* Tap route with dynamic user ID */}
+        //         <Route path={`${TAP}:user_id/*`} element={<Layout path="tap" />} />
+        //     </Routes>
+        // </BrowserRouter>
     );
 }
 
