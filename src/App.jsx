7e7a95ee1879a7aebd6f749d9c3238cc9e06b533
ibrohim74 <xibroxim@gmail.com>
2024-09-20 19:15:06 +0500

    sdsad

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Navigate, Route, Routes, useParams} from "react-router-dom";
 import {FOOTBALL, INDEX, TAP} from "./utils/const.jsx";
 import IndexPage from "./pages/index_page.jsx";
 import Layout from "./pages/layout.jsx";
@@ -9,19 +9,19 @@ function App() {
     useEffect(() => {
         tg.expand()
     }, [])
-
+const {user_id} = useParams();
     return (
         <BrowserRouter>
             <Routes>
                 {/*<Route path="/" element={<Navigate to={`${INDEX}123456`} />} />*/}
                 {/* Index page route */}
-                <Route path={`${INDEX}:user_id`} element={<IndexPage />} />
+                <Route path={`${INDEX}${user_id}`} element={<IndexPage />} />
 
                 {/* Football route with dynamic user ID */}
-                <Route path={`${FOOTBALL}:user_id/*`} element={<Layout path="football" />} />
+                <Route path={`${FOOTBALL}${user_id}/*`} element={<Layout path="football" />} />
 
                 {/* Tap route with dynamic user ID */}
-                <Route path={`${TAP}:user_id/*`} element={<Layout path="tap" />} />
+                <Route path={`${TAP}${user_id}/*`} element={<Layout path="tap" />} />
             </Routes>
         </BrowserRouter>
     );
