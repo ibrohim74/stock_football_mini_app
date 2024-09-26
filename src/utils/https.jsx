855e48d41fca13604108eabb1d12d80298470620
ibrohim74 <xibroxim@gmail.com>
2024-09-27 01:14:19 +0500

    sdsad

--- src/utils/https.jsx
+++ src/utils/https.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const $API = axios.create({
     // baseURL: 'https://yengi.mussi.uz/',
-    baseURL: 'http://192.168.100.149:8000/',
+    baseURL: 'https://yengi.mussi.uz/',
     headers: {
         'Content-Type': 'application/json',
     },
