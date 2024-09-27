51a5508bb5c4061702413de56d7d01e0cacf2bd6
ibrohim74 <xibroxim@gmail.com>
2024-09-27 16:43:34 +0500

    sdsad

--- src/pages/Events_Page/events.jsx
+++ src/pages/Events_Page/events.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
 import './events.css';
 import gift from '../../assets/icon/freepik-export-20240923164119B0Nu.webp';
@@ -6,29 +6,41 @@ import ball from '../../assets/icons/soccer_ball.png';
 import ready from '../../assets/icon/success.webp';
 import ongoing from '../../assets/icon/restart.webp';
 import active from '../../assets/icon/spark.webp';
-import { Link, useParams } from 'react-router-dom';
+import {Link, useParams} from 'react-router-dom';
 import Collapse_events from '../../component/collapse_events/collapse_events.jsx';
 import {message} from "antd";
 
 const Events = () => {
-    const { user_id } = useParams();
+    const {user_id} = useParams();
     const [messageApi, contextHolder] = message.useMessage();
 
     const [eventsData, setEventsData] = useState([
-        { id: 1, event: 'event task obuna boling1', event_bonus: '+5M', event_link: 'https://test.uz', status: 'active' },
-        { id: 2, event: 'event task obuna boling2', event_bonus: '+5M', event_link: 'https://test.uz', status: 'ongoing' },
-        { id: 3, event: 'event task obuna boling3', event_bonus: '+5M', event_link: 'https://test.uz', status: 'ready' },
-        { id: 4, event: 'stock football ga obuna boling4', event_bonus: '+5M', event_link: 'https://test.uz', status: 'ready' }
+        {id: 1, event: 'event task obuna boling1', event_bonus: '+5M', event_link: 'https://test.uz', status: 'active'},
+        {
+            id: 2,
+            event: 'event task obuna boling2',
+            event_bonus: '+5M',
+            event_link: 'https://test.uz',
+            status: 'ongoing'
+        },
+        {id: 3, event: 'event task obuna boling3', event_bonus: '+5M', event_link: 'https://test.uz', status: 'ready'},
+        {
+            id: 4,
+            event: 'stock football ga obuna boling4',
+            event_bonus: '+5M',
+            event_link: 'https://test.uz',
+            status: 'ready'
+        }
         // Add more events as necessary...
     ]);
 
     const startTask = (eventId) => {
-        setEventsData(eventsData.map(event => event.id === eventId ? { ...event, status: 'ongoing' } : event));
+        setEventsData(eventsData.map(event => event.id === eventId ? {...event, status: 'ongoing'} : event));
         messageApi.success("vazifani bajarish boshlandi")
     };
 
     const cancelTask = (eventId) => {
-        setEventsData(eventsData.map(event => event.id === eventId ? { ...event, status: 'active' } : event));
+        setEventsData(eventsData.map(event => event.id === eventId ? {...event, status: 'active'} : event));
         messageApi.error("vazifani bekor qilindi")
     };
 
@@ -36,30 +48,30 @@ const Events = () => {
         key: item.id.toString(),
         label: (
             <div className="events_item" key={item.id}>
-                <img src={gift} loading="lazy" alt="logo" className="events_item_logo" />
+                <img src={gift} loading="lazy" alt="logo" className="events_item_logo"/>
                 <div className="events_item_text">
                     <p>{item.event}</p>
                     <span>
-            <img loading="lazy" src={ball} alt="ball" />
+            <img loading="lazy" src={ball} alt="ball"/>
             <p>{item.event_bonus}</p>
           </span>
                 </div>
                 <span className="events_item_status">
-          {item.status === 'active' && <img src={active} loading="lazy" alt="active" />}
-                    {item.status === 'ready' && <img src={ready} loading="lazy" alt="ready" />}
-                    {item.status === 'ongoing' && <img src={ongoing} loading="lazy" alt="ongoing" />}
+          {item.status === 'active' && <img src={active} loading="lazy" alt="active"/>}
+                    {item.status === 'ready' && <img src={ready} loading="lazy" alt="ready"/>}
+                    {item.status === 'ongoing' && <img src={ongoing} loading="lazy" alt="ongoing"/>}
         </span>
             </div>
         ),
         children: (
             <div className="event_collapse_box">
                 <div className="img_event_collapse">
-                    <img src={gift} alt="gift" />
+                    <img src={gift} alt="gift"/>
                 </div>
                 <div className="text_event_collapse">
                     <h1>{item.event}</h1>
                     <span>
-            <img loading="lazy" src={ball} alt="ball" />
+            <img loading="lazy" src={ball} alt="ball"/>
             <p>{item.event_bonus}</p>
           </span>
                 </div>
@@ -91,22 +103,22 @@ const Events = () => {
                             <h1>Kundalik vazifalar</h1>
                         </div>
                         <Link className="events_item" to={`/${user_id}/quiz`}>
-                            <img src={gift} loading="lazy" alt="logo" className="events_item_logo" />
+                            <img src={gift} loading="lazy" alt="logo" className="events_item_logo"/>
                             <div className="events_item_text">
                                 <p>Asosiy vazifa</p>
                                 <span>
-                  <img loading="lazy" src={ball} alt="ball" />
+                  <img loading="lazy" src={ball} alt="ball"/>
                   <p>5k</p>
                 </span>
                             </div>
                             <span className="events_item_status">
-                <img loading="lazy" src={active} alt="active" />
+                <img loading="lazy" src={active} alt="active"/>
               </span>
                         </Link>
                         <div className="events_box_content_title">
                             <h1>Vazifalar ro'yxati</h1>
                         </div>
-                        <Collapse_events items={CollapseItem} />
+                        <Collapse_events items={CollapseItem}/>
                     </div>
                 </div>
             </div>
