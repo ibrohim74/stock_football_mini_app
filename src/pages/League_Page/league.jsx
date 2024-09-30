7299d6c86d844c192219e3f69df2a5ae4b959681
ibrohim74 <xibroxim@gmail.com>
2024-10-01 00:04:17 +0500

    sdsad

--- src/pages/League_Page/league.jsx
+++ src/pages/League_Page/league.jsx
@@ -1,30 +1,28 @@
 import React, { useEffect } from 'react';
 import LeagueScroll from './component/league_scroll.jsx';
-import {Link, useNavigate, useParams} from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 const League = () => {
     const { user_id } = useParams();
-    const navigate = useNavigate();
 
     useEffect(() => {
         const backButton = window.Telegram.WebApp.BackButton;
         backButton.show();
 
-        // Correcting the event listener assignment
+        // Corrected the onClick event to use window.location.href
         backButton.onClick = () => {
-            return <Link to={`/${user_id}`}>sad</Link>
+            window.location.href = `/${user_id}`;
         };
 
         // Cleanup: hide the back button on component unmount
         return () => {
             backButton.hide();
         };
-    }, [navigate, user_id]);
+    }, [user_id]);
 
     return (
         <>
             <LeagueScroll />
-            {/* Optional: BackTab component */}
         </>
     );
 };
