87bf398d0bec5b6fe2a5e79e7f920a5c8506e653
ibrohim74 <xibroxim@gmail.com>
2024-09-30 23:51:51 +0500

    sdsad

--- src/pages/League_Page/league.jsx
+++ src/pages/League_Page/league.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import LeagueScroll from './component/league_scroll.jsx';
 import BackTab from "../../component/backTab/BackTab.jsx";
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 
 const League = () => {
-
+    const {user_id} = useParams();
+    const navigate = useNavigate();
+    const backButton = window.Telegram.WebApp.BackButton
+    backButton.show();
+    backButton.onclick(()=>{
+        navigate(`/${user_id}`)
+    })
     return (
         <>
 
