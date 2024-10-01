ce0d5d29e07ea5be8c1428b4ffb179b5501eb519
ibrohim74 <xibroxim@gmail.com>
2024-10-01 15:31:25 +0500

    sdsad

--- src/pages/League_Page/league.jsx
+++ src/pages/League_Page/league.jsx
@@ -1,15 +1,25 @@
 import React, { useEffect } from 'react';
 import LeagueScroll from './component/league_scroll.jsx';
-import {useNavigate, useParams} from "react-router-dom";
+import { useNavigate, useParams } from 'react-router-dom';
 
 const League = () => {
     const { user_id } = useParams();
     const navigate = useNavigate();
+
     useEffect(() => {
-        window.Telegram.WebApp.onEvent("backButtonClicked" , ()=>{
-            navigate(`/${user_id}`)
-        })
-    }, []);
+        // BackButton ni ko'rsatish
+        window.Telegram.WebApp.BackButton.show();
+
+        // "backButtonClicked" hodisasiga qo'shish
+        window.Telegram.WebApp.onEvent("backButtonClicked", () => {
+            navigate(`/${user_id}`);
+        });
+
+        // Komponent unmount bo'lganida voqeani tozalash
+        return () => {
+            window.Telegram.WebApp.offEvent("backButtonClicked");
+        };
+    }, [navigate, user_id]);
 
     return (
         <>
